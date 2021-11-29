121335f04ccbd872bd6d89a681379976f03cdeea
booleangabs <62074916+booleangabs@users.noreply.github.com>
2021-11-28 23:47:06 -0300

    Python insertion sort update

--- Sort/Python/insertion_sort.py
+++ Sort/Python/insertion_sort.py
@@ -1,8 +1,6 @@
 def insertionSort(array: list):
     for i in range(1, len(array)):
-        current = array[i]
-        idx = i - 1
-        while (idx >= 0) & (current < array[idx]):
-            array[idx + 1] = array[idx]
-            idx -= 1
-        array[idx + 1] = current
+        j = i
+        while (j > 0) & (array[j - 1] > array[j]):
+            array[j], array[j - 1] = array[j - 1], array[j]
+            j -= 1
